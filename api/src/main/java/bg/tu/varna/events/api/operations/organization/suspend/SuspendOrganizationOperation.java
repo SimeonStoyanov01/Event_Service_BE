4fabe526e0c43fe467a9d43b2c83fc9912715f86
SimeonStoyanov01 <simosstoyanov@gmail.com>
2024-06-10 19:55:37 +0300

    Suspend organization operation added

---
+++ api/src/main/java/bg/tu/varna/events/api/operations/organization/suspend/SuspendOrganizationOperation.java
@@ -0,0 +1,7 @@
+package bg.tu.varna.events.api.operations.organization.suspend;
+
+import bg.tu.varna.events.api.base.Processor;
+
+public interface SuspendOrganizationOperation extends Processor<SuspendOrganizationResponse, SuspendOrganizationRequest> {
+
+}
