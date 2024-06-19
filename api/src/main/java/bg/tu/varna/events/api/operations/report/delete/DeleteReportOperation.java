84143b2040c440ffad6d38c4bcfb7b62269bcf08
SimeonStoyanov01 <simosstoyanov@gmail.com>
2024-06-19 10:54:36 +0300

    Delete report operation

---
+++ api/src/main/java/bg/tu/varna/events/api/operations/report/delete/DeleteReportOperation.java
@@ -0,0 +1,7 @@
+package bg.tu.varna.events.api.operations.report.delete;
+
+import bg.tu.varna.events.api.base.Processor;
+
+public interface DeleteReportOperation extends Processor<DeleteReportResponse, DeleteReportRequest> {
+
+}
